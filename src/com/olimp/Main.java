9be53a606bcf6c3c5a452eea2282ec89d8edc24d
Ayder Dev <ayder.dev@gmail.com>
2019-11-24 01:55:28 +0300

    T2 are the same square arrays

--- src/com/olimp/Main.java
+++ src/com/olimp/Main.java
@@ -1,5 +1,9 @@
 package com.olimp;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 import static org.junit.Assert.assertEquals;
 
 public class Main {
@@ -9,7 +13,36 @@ public class Main {
     }
 
     private static void Tests() {
-        assertEquals("", "", "");
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        assertEquals(true, AreSame.comp(a, b));
+        a = new int[]{12, 11, 11};
+        b = new int[]{121, 144, 121};
+        assertEquals(true, AreSame.comp(a, b));
+    }
+
+    public static class AreSame {
+
+        public static boolean comp(int[] a, int[] b) {
+            if (a == null || b == null || a.length != b.length) {
+                return false;
+            }
+            HashMap<Integer, Integer> squares = new HashMap<>(a.length);
+            for (int num : a) {
+                Integer square = num * num;
+                Integer count = squares.get(square);
+                squares.put(square, count != null ? count + 1 : 1);
+            }
+            for (int square : b) {
+                Integer count = squares.get(square);
+                if (count == null || count == 0) {
+                    return false;
+                } else {
+                    squares.put(square, count - 1);
+                }
+            }
+            return true;
+        }
     }
 
 }
