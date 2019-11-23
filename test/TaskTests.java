e848ff716b96a6fb4d75a8bd9165e74a4af55353
Ayder Dev <ayder.dev@gmail.com>
2019-11-24 01:17:59 +0300

    Codewars template console application

---
+++ test/TaskTests.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskTests {
+        @Test
+        public void test1() {
+            int a = 2 + 2;
+            int b = 4;
+            assertEquals(true, a == b);
+        }
+}
