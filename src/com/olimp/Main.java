7be2141164f2ba63873b165207642f9142650064
Ayder Alm <send.ayder@gmail.com>
2019-12-05 21:07:57 +0000

    T10 implemented count factorial trailing zeroes https://www.codewars.com/kata/number-of-trailing-zeros-of-n/java

--- src/com/olimp/Main.java
+++ src/com/olimp/Main.java
@@ -1,15 +1,32 @@
 package com.olimp;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.IntStream;
 
+/**
+ * Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+ *
+ * N! = 1 * 2 * 3 * ... * N
+ *
+ * Be careful 1000! has 2568 digits...
+ *
+ * Examples
+ * zeros(6) = 1
+ * # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+ *
+ * zeros(12) = 2
+ * # 12! = 479001600 --> 2 trailing zeros
+ */
 public class Main {
 
     public static void main(String[] args) {
-        Tests();
+        System.out.println(Solution.zeros(12));
     }
 
-    private static void Tests() {
-        assertEquals("", "", "");
+    public static class Solution {
+        public static int zeros(int n) {
+            int max5Power = (int) (Math.log(n) / Math.log(5));
+            return IntStream.range(1, max5Power + 1).map(i -> (int) (n/Math.pow(5, i))).sum();
+        }
     }
 
 }
