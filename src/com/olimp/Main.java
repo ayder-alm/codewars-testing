c4c5aea8f20bc75ff45b4f07d3a5eb6666327c4c
Ayder Alm <send.ayder@gmail.com>
2019-12-05 20:14:34 +0000

    T8 count ip address range https://www.codewars.com/kata/count-ip-addresses/java

--- src/com/olimp/Main.java
+++ src/com/olimp/Main.java
@@ -1,15 +1,36 @@
 package com.olimp;
 
-import static org.junit.Assert.assertEquals;
+import java.util.stream.IntStream;
+
+/**
+ *
+ *Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+ *
+ * All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+ *
+ * Examples
+ * ips_between("10.0.0.0", "10.0.0.50")  ==   50
+ * ips_between("10.0.0.0", "10.0.1.0")   ==  256
+ * ips_between("20.0.0.10", "20.0.1.0")  ==  246
+ *
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-        Tests();
+        long count = CountIPAddresses.ipsBetween("10.0.0.0", "20.0.1.1");
+        System.out.println(count);
     }
 
-    private static void Tests() {
-        assertEquals("", "", "");
+    public static class CountIPAddresses {
+
+        public static long ipsBetween(String start, String end) {
+            String[] starts = start.split("\\.");
+            String[] ends = end.split("\\.");
+            return IntStream.range(0, 4)
+                    .mapToLong(i -> (long) ((Integer.parseInt(ends[i]) - Integer.parseInt(starts[i])) * Math.pow(256, 3 - i)))
+                    .sum();
+        }
     }
 
 }
--- test/TaskTests.java
+++ test/TaskTests.java
@@ -1,12 +1,12 @@
 import org.junit.Test;
+import com.olimp.Main.CountIPAddresses;
 
 import static org.junit.Assert.assertEquals;
 
 public class TaskTests {
-        @Test
-        public void test1() {
-            int a = 2 + 2;
-            int b = 4;
-            assertEquals(true, a == b);
-        }
+    @Test
+    public void ipsBetween() throws Exception {
+        assertEquals( 50, CountIPAddresses.ipsBetween( "10.0.0.0", "10.0.0.50" ) );
+        assertEquals( 246, CountIPAddresses.ipsBetween( "20.0.0.10", "20.0.1.0" ) );
+    }
 }
