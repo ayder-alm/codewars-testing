c1fa1264c949fedaf23f36aada962c307eebc433
Ayder Alm <send.ayder@gmail.com>
2020-02-17 19:32:54 +0300

    T16 - implemented randomizer guessing game https://www.codewars.com/kata/54bd79a7956834e767001357/train/java

---
+++ src/main/java/com/olimp/Psychic.java
@@ -0,0 +1,7 @@
+package com.olimp;
+
+public class Psychic {
+    public static double guess() {
+        return java.lang.Math.random();
+    }
+}
--- src/test/java/com/olimp/TaskTests.java
+++ src/test/java/com/olimp/TaskTests.java
@@ -5,10 +5,8 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class TaskTests {
-        @Test
-        public void mainTest() {
-            int a = 2 + 2;
-            int b = 4;
-            assertEquals(true, a == b);
-        }
+    @Test
+    public void testRandom() {
+        assertEquals(Psychic.guess(), java.lang.Math.random(), 0);
+    }
 }
